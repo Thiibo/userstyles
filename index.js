8c0f4948442846ff46c6cd4aa9576cccb93447ba
Thiibo <Thiibo@users.noreply.github.com>
2020-06-26 14:13:32 +0200

    Added methods

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ $header = document.getElementsByTagName("header")[0];
 $logo = document.getElementById("logo");
 $logo.style.fontSize = $header.offsetHeight / 2 - 20 + "px";
 
+// Select method
+$methods = document.getElementsByClassName('method');
+
+for (let i = 0; i < $methods.length; i++) {
+  let $el = $methods.item(i);
+  $el.addEventListener('click', function() {
+    for (var i = 0; i < $methods.length; i++) $methods.item(i).classList.remove('selected');
+    this.classList.add('selected');
+  });
+}
+
+
 // Select userstyle
 $userstyles = document.getElementsByClassName('userstyle');
 
@@ -10,6 +22,6 @@ for (let i = 0; i < $userstyles.length; i++) {
   let $el = $userstyles.item(i);
   $el.addEventListener('click', function() {
     for (var i = 0; i < $userstyles.length; i++) $userstyles.item(i).classList.remove('selected');
-    $el.classList.add('selected');
+    this.classList.add('selected');
   });
 }
